ccbd345183fe54d74777c6d0d2e665e05e1dd33b
sagar kumar <94098920+Sagar-Kumar01@users.noreply.github.com>
2025-11-27 14:24:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-//your JS code here. If required.
+
 let signup_form = document.getElementById("signup-form");
 let name = document.getElementById("name");
 let email = document.getElementById("email");
 let password = document.getElementById("password");
 let confirm_password = document.getElementById("confirm-password");
-let submit = document.getElementById("submit");
 
-submit.addEventListener("click",(e)=>{
-	e.preventDefault();
 
-	let pass = password.value;
-	let pass2 = confirm_password.value;
+signup_form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-	if(pass == pass2){
-		sessionStorage.setItem("name",name.target.value);
-		sessionStorage.setItem("email",email.target.value);
-		sessionStorage.setItem("password",password.target.value);
-		alert("Sign up successful!");
-		signup_form.submit();
-	}else{
-		alert("Passwords do not match");
-	}
-})
+    let pass = password.value.trim();
+    let pass2 = confirm_password.value.trim();
+
+    if (pass === pass2) {
+        sessionStorage.setItem("name", name.value.trim());
+        sessionStorage.setItem("email", email.value.trim());
+        sessionStorage.setItem("password", pass);
+
+        alert("Sign up successful!");
+		signup_form.reset();
+    } else {
+        alert("Passwords do not match");
+    }
+});
